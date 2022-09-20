b1013c3e3ab87016b21e2961442e4aa0d772e2cc
devAyushDubey <devAyushDubey@users.noreply.github.com>
2022-09-20 11:52:29 +0000

    :art: Python code fromated with psf/black

--- pastebin-bot/insta_all_pictures.py
+++ pastebin-bot/insta_all_pictures.py
@@ -1,4 +1,4 @@
-from instaloader import Instaloader,Profile
+from instaloader import Instaloader, Profile
 import threading
 import shutil
 import os
@@ -10,42 +10,49 @@ load_dotenv()
 status = ""
 isLoggedIn = False
 
+
 def get_status():
     return status
 
+
 profile_instance = Instaloader()
+
 
 def login():
     global profile_instance
     global isLoggedIn
-    
+
     name = os.getenv("username")
     passw = os.getenv("password")
-    profile_instance.login(name,passw)
+    profile_instance.login(name, passw)
     isLoggedIn = True
-    
+
 
 def fetch_posts(user):
 
     global status
     status = ""
-    
 
     try:
         prof = Profile.from_username(profile_instance.context, user)
         print(prof)
         posts = prof.get_posts()
-        if(posts.count > 100):
+        if posts.count > 100:
             status = "limit_exceeded"
             return
     except:
         raise FileNotFoundError
-    
 
     for post in posts:
         try:
-            time.sleep(random.randint(0,5)/10.0)
-            task = threading.Thread(target=profile_instance.download_post, args=(post,user,))
+            time.sleep(random.randint(0, 5) / 10.0)
+            task = threading.Thread(
+                target=profile_instance.download_post,
+                args=(
+                    post,
+                    user,
+                ),
+            )
             task.start()
         except:
             print("Skipping due to status code 401")
